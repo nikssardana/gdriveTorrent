8bb761900b187cc23b5f4cd94b8000468ca9db4c
Nikhil Sardana <nikhilklk@gmail.com>
2017-09-27 22:10:01 +0530

    start subprocess for aria2c and show status; -d may be not working properly, as directory is not getting selected

--- gdriveTorrent/mainApp/views.py
+++ gdriveTorrent/mainApp/views.py
@@ -5,13 +5,22 @@ from django.shortcuts import render
 from mainApp.forms import *
 from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
+import os
+from subprocess import Popen
+import subprocess
 
 @login_required
 def downloadView(request):
     dictV = {}
     if request.method == 'POST':
         torrentUrl = request.POST.get('torrentUrl')
-        print "URL",torrentUrl
+        #TODO: Change directory on server
+        script = 'aria2c "%s -d /home/nikhil/Programs/Django/gdriveTorrent/gdriveTorrent/downloads/ &"'%(torrentUrl)
+
+        process = Popen(script, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        dictV['status'] = 'Your torrent will be downloaded shortly...'
+
     return render(request, 'download.html', dictV)
 
 def loginView(request):
